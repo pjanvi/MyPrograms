bf9bf92444c1bdd917a713c652b708f48597c2f2
Janvi <janvi.p@pm.me>
2019-11-02 20:05:32 +0530

    Added personal info program

---
+++ 02-lesson/01-PersonalInformation/personal.py
@@ -0,0 +1,12 @@
+#name
+#address, with city, state, and ZIP
+#telephone number
+#college major
+
+def personal_info():
+    print("Name : 'John Doe'")
+    print("Address: 'Malavya Nagar'\n'Nellore'\n'Andhra Pradesh'\n524001")
+    print("Telephone number: 127436473") 
+    print("College major: 'Btech'")
+
+personal_info()
